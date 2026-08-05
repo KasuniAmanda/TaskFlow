57f4eac38299180e018c4670a34dba827ad1a56e
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2026-08-05 15:33:27 +0530

    feat: add server setup with Express and CORS middleware

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test API
+app.get("/", (req, res) => {
+
+    res.send("TaskFlow API is running");
+
+});
+
+
+// Server Port
+const PORT = 5000;
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
+
+});
